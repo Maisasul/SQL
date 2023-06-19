d831640d43abddbef4d4e46371cb527741d19692
Maisasul <104488163+Maisasul@users.noreply.github.com>
2023-06-19 11:50:39 +0400

    Extension

---
+++ Extension/Extension/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Extension
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 12345;
+            int y = x.Mirror();
+            Console.WriteLine(y);
+
+            List<int> list = new List<int> { 1,2,3,4,5,6,7,8};
+            var r = list.Where(i => i % 2 == 0);
+            foreach(var item in r)
+            {
+                Console.WriteLine(item + ",");
+            }
+            Console.WriteLine("");
+
+            var sql = from i in list
+                      where i >= 5
+                      select i;
+            foreach (var item in sql)
+            {
+                Console.WriteLine(item + ",");
+            }
+        }
+    }
+}
---
+++ Extension/Extension/int32ex.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extension
+{
+    static class int32ex
+    {
+        public static int Mirror(this int x)
+        {
+            char[] z = x.ToString().ToCharArray();
+            Array.Reverse(z);
+            return int.Parse(new string(z));
+        }
+    }
+}
